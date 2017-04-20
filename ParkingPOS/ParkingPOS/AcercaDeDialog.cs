fcdad5964a75d04981abb59897ecc0db9758818a
Erick G. Islas-Osuna <erickisos653@gmail.com>
2017-04-20 00:43:22 -0500

    Created Icon File using GIMP

--- ParkingPOS/ParkingPOS/AcercaDeDialog.cs
+++ ParkingPOS/ParkingPOS/AcercaDeDialog.cs
@@ -2,10 +2,15 @@
 
 namespace ParkingPOS
 {
-	public partial class AcercaDeDialog : Gtk.Dialog
+	public partial class AcercaDeDialog : Gtk.AboutDialog
 	{
 		public AcercaDeDialog ()
 		{
+			this.ProgramName = "Parking POS";
+			this.Version = "alpha1";
+			this.Logo = new Gdk.Pixbuf("img/icon.png");
+			this.Comments = @"Prueba de Conexion entre ventanas";
+			this.Website = "https://github.com/erickisos/ParkingPOS";
 			this.Build ();
 		}
 	}
